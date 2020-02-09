3d80d0cf604ae0e4f403738f598a4d2fc1a08515
Cixtor <cixtords@gmail.com>
2020-02-09 09:00:00 -0800

    Add unit-test to check if the cookie page offsets are correct

--- test0_test.go
+++ test0_test.go
@@ -19,4 +19,14 @@ func checkCookiePage(t *testing.T, data []byte, index int, expected Page) {
 	if cook.pages[index].Length != expected.Length {
 		t.Fatalf("incorrect number of cookies at pages[%d]\n- %d\n+ %d", index, expected.Length, cook.pages[index].Length)
 	}
+
+	if len(cook.pages[index].Offsets) != len(expected.Offsets) {
+		t.Fatalf("incorrect number of cookie offsets at pages[%d]\n- %#v\n+ %#v", index, expected.Offsets, cook.pages[index].Offsets)
+	}
+
+	for idx, offset := range expected.Offsets {
+		if cook.pages[index].Offsets[idx] != offset {
+			t.Fatalf("incorrect cookie offset at pages[%d]\n- %#v\n+ %#v\n! check index #%d", index, expected.Offsets, cook.pages[index].Offsets, idx)
+		}
+	}
 }
