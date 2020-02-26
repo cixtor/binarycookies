dbd9dee52545e2e374e22cf823e1207a9b2a1a10
Cixtor <cixtords@gmail.com>
2020-02-26 09:00:00 -0800

    Add instructions to the HexFiend template to read page offsets

--- BinaryCookies.tcl
+++ BinaryCookies.tcl
@@ -9,3 +9,15 @@ section "Header" {
 		uint32 "Page Offset #$pageOffset"
 	}
 }
+
+for {set pageIndex 0} {$pageIndex < $size} {incr pageIndex} {
+	section "Page #$pageIndex" {
+		little_endian
+		bytes 4 "Page Tag"
+		set numCookies [uint32 "Num. Cookies"]
+		for {set i 1} {$i <= $numCookies} {incr i} {
+			uint32 "Cookie Offset #$i"
+		}
+		bytes 4 "Page End"
+	}
+}
