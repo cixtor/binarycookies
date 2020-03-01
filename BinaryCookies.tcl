27a0a5162907601670e022974c2a853220a77c09
Cixtor <cixtords@gmail.com>
2020-03-01 09:00:00 -0800

    Add instructions to the HexFiend template to read optional bplist

--- BinaryCookies.tcl
+++ BinaryCookies.tcl
@@ -50,4 +50,29 @@ for {set pageIndex 0} {$pageIndex < $size} {incr pageIndex} {
 
 section "Footer" {
 	bytes 8 "Checksum"
+
+	# Additional bytes are part of an optional bplist dictionary that usually
+	# contains a dictionary with a key NSHTTPCookieAcceptPolicy and a value
+	# representing the cookie accept policy for all tasks within sessions based
+	# on this configuration.
+	#
+	# Example:
+	#
+	# $ hexdump -v -C Cookies.binarycookies | tail -n5
+	# 00000000  62 70 6c 69 73 74 30 30  d1 01 02 5f 10 18 4e 53  |bplist00..._..NS|
+	# 00000010  48 54 54 50 43 6f 6f 6b  69 65 41 63 63 65 70 74  |HTTPCookieAccept|
+	# 00000020  50 6f 6c 69 63 79 10 02  08 0b 26 00 00 00 00 00  |Policy....&.....|
+	# 00000030  00 01 01 00 00 00 00 00  00 00 03 00 00 00 00 00  |................|
+	# 00000040  00 00 00 00 00 00 00 00  00 00 28                 |..........(|
+	# 0000004b
+	#
+	# $ plutil -p Tail.plist
+	# {
+	#   "NSHTTPCookieAcceptPolicy" => 2
+	# }
+	if ![end] {
+		big_endian
+		set bplistSize [uint32 "bplist size"]
+		bytes $bplistSize "bplist content"
+	}
 }
